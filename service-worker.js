9efa3f7a1a6b284b2a25587e1fcb8dfbe61abdf2
f554bk6gmt-spec <f554bk6gmt@privaterelay.appleid.com>
2026-04-25 15:27:12 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE = 'locallead-ai-v3-accessible-login';
+const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
